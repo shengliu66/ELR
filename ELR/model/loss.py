513693ebba2fbb22c9a842fe0a3601e38d402246
Sheng Liu <67348655+shengliu66@users.noreply.github.com>
2020-07-05 16:27:11 -0400

    Update loss.py

--- ELR/model/loss.py
+++ ELR/model/loss.py
@@ -9,20 +9,20 @@ def cross_entropy(output, target):
 
 
 class elr_loss(nn.Module):
-    def __init__(self, num_examp, num_classes=10, alpha=0.3):
+    def __init__(self, num_examp, num_classes=10, beta=0.3):
         super(elr_loss, self).__init__()
         self.num_classes = num_classes
         self.config = ConfigParser.get_instance()
         self.USE_CUDA = torch.cuda.is_available()
         self.target = torch.zeros(num_examp, self.num_classes).cuda() if self.USE_CUDA else torch.zeros(num_examp, self.num_classes)
-        self.alpha = alpha
+        self.beta = beta
         
 
     def forward(self, index, output, label):
         y_pred = F.softmax(output,dim=1)
         y_pred = torch.clamp(y_pred, 1e-4, 1.0-1e-4)
         y_pred_ = y_pred.data.detach()
-        self.target[index] = self.alpha * self.target[index] + (1-self.alpha) * ((y_pred_)/(y_pred_).sum(dim=1,keepdim=True))
+        self.target[index] = self.beta * self.target[index] + (1-self.beta) * ((y_pred_)/(y_pred_).sum(dim=1,keepdim=True))
         ce_loss = F.cross_entropy(output, label)
         elr_reg = ((1-(self.target[index] * y_pred).sum(dim=1)).log()).mean()
         final_loss = ce_loss +  self.config['train_loss']['args']['lambda']*elr_reg
